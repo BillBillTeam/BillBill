6999bda2f490431ecfaab763f2e66394f0183e6e
LiZeC <tomgreem123@gmail.com>
2017-10-14 09:33:57 +0900

    Add new Db Test

---
+++ app/src/main/java/lz/db/BaseDb.java
@@ -0,0 +1,45 @@
+package lz.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by LiZeC on 2017/10/14.
+ * 基础测试数据库样板，不作为实际使用的类
+ *
+ */
+
+public class BaseDb extends SQLiteOpenHelper {
+    private String dbName = "Bdb";
+    private String darkRen;
+
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     * This method always returns very quickly.  The database is not actually
+     * created or opened until one of {@link #getWritableDatabase} or
+     * {@link #getReadableDatabase} is called.
+     *
+     * @param context to use to open or create the database
+     * @param name    of the database file, or null for an in-memory database
+     * @param factory to use for creating cursor objects, or null for the default
+     * @param version number of the database (starting at 1); if the database is older,
+     *                {@link #onUpgrade} will be used to upgrade the database; if the database is
+     *                newer, {@link #onDowngrade} will be used to downgrade the database
+     */
+    public BaseDb(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
