22a992993d5285de44af86646abd108991fd93d7
lrStyle <350552948@qq.com>
2017-11-01 23:58:28 +0800

    重做了UI控件

--- app/src/main/java/fivene/billbill/BillListActivity.java
+++ app/src/main/java/fivene/billbill/BillListActivity.java
@@ -3,25 +3,32 @@ package fivene.billbill;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class BillListActivity extends AppCompatActivity {
-  private   LinearLayout ll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_list);
-        ll=(LinearLayout) findViewById(R.id.bill_list_layout);
-        Button bt = new Button(this);
-        ll.addView(bt);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(BillListActivity.this,StatisticsActivity.class);
-                BillListActivity.this.startActivity(intent);
-            }
-        });
+        //解析
+        final LayoutInflater inflater = LayoutInflater.from(this);
+
+         // 获取需要被添加控件的布局
+        final LinearLayout lin = (LinearLayout) findViewById(R.id.bill_list_layout);
+
+         // 获取需要添加的布局
+        LinearLayout b_c_layout = (LinearLayout) inflater.inflate(
+                R.layout.bill_column,null).findViewById(R.id.bill_column_layout);
+        LinearLayout b_i_layout = (LinearLayout) inflater.inflate(
+                R.layout.bill_item, null).findViewById(R.id.bill_item_layout);
+
+         // 将布局加入到当前布局中(动态)
+        lin.addView(b_c_layout);
+        lin.addView(b_i_layout);
+
     }
 }
