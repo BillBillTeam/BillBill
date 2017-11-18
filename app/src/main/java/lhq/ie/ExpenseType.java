c1f0ce22897ec9b791752fda38da5b24ddd30fc6
huiqingli <1259701203@qq.com>
2017-11-18 16:25:07 +0800

    修改了消费类型的一些函数

--- app/src/main/java/lhq/ie/ExpenseType.java
+++ app/src/main/java/lhq/ie/ExpenseType.java
@@ -193,14 +193,14 @@ public class ExpenseType {
      */
     public void exchange(int Index1,int Index2)
     {
-        for(int i=0;i<=list.size();i++)
+        for(int i=0;i<list.size();i++)
         {
             if(list.get(i).getIndex()==Index1)
             {
                 list.get(i).setIndex(Index2);
             }
         }
-        for(int i=0;i<=list.size();i++)
+        for(int i=0;i<list.size();i++)
         {
             if(list.get(i).getIndex()==Index2)
             {
