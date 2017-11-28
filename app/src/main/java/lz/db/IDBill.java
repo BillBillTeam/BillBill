a335860027a3707760fcb36f8e582419d9c59424
LiZeC <tomgreem123@gmail.com>
2017-11-28 14:40:38 +0800

    删除实际上没有效果的子类重载

--- app/src/main/java/lz/db/IDBill.java
+++ app/src/main/java/lz/db/IDBill.java
@@ -18,69 +18,9 @@ public class IDBill extends Bill {
     }
 
     @Override
-    public int getYear() {
-        return super.getYear();
-    }
-
-    @Override
-    public int getMonth() {
-        return super.getMonth();
-    }
-
-    @Override
-    public int getDay() {
-        return super.getDay();
-    }
-
-    @Override
-    public String getType() {
-        return super.getType();
-    }
-
-    @Override
-    public double getAmount() {
-        return super.getAmount();
-    }
-
-    @Override
-    public String getRemark() {
-        return super.getRemark();
-    }
-
-    @Override
     public String toString() {
         return "IDBill{" +
                 "ID=" + ID + " " +super.toString()+
                 '}';
-    }
-
-    @Override
-    public void setYear(int year) {
-        super.setYear(year);
-    }
-
-    @Override
-    public void setMonth(int month) {
-        super.setMonth(month);
-    }
-
-    @Override
-    public void setDay(int day) {
-        super.setDay(day);
-    }
-
-    @Override
-    public void setType(String type) {
-        super.setType(type);
-    }
-
-    @Override
-    public void setAmount(double amount) {
-        super.setAmount(amount);
-    }
-
-    @Override
-    public void setRemark(String remark) {
-        super.setRemark(remark);
     }
 }
